cdbb190215b4d30a0fd217d75ccdaf0cb35f30c3
thasup <thanachonfirst@hotmail.com>
2024-12-29 16:57:59 +0700

    chore: Configure maqe eslint plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,30 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+import maqe from "eslint-config-maqe";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: {
+    settings: {
+      react: "detect"
+    }
+  }
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...fixupConfigRules(
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ),
+  {
+    plugins: {
+      maqe: fixupPluginRules(maqe)
+    }
+  }
 ];
 
 export default eslintConfig;
